706bf8ad2aab20258e07e6143ede218619a7726f
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2012-12-05 15:04:55 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WoSporkSampleApp::Application.config.secret_token = 'e7dff1bd889fab38c08e9ed60adbcb0b6a2b4018c8b3f0cbc5380cc0a64a22d9187e73e2e735cf7beaceb5861ec6982f54c4a0c8363f3b2fed90b1fbb915193b'
