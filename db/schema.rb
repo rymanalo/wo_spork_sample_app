0490cab5a852942a3b952223574c4f2ebe25e1b4
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2012-12-09 23:56:23 -0800

    Finish user edit, update, index, and destroy actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121206072947) do
+ActiveRecord::Schema.define(:version => 20121210074216) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20121206072947) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin"
   end
 
 end
