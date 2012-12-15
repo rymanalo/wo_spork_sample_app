b3b71fac03fc8a58b3b27b7bd5bb1968bc11359b
Ryan Manalo <ryan.manalo@alum.berkeley.edu>
2012-12-15 11:30:31 -0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,20 +5,31 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
-    @user = User.new
+    if signed_in?
+      flash[:success] = "you are already signed in"
+      redirect_to root_path
+    else
+      @user = User.new
+    end
   end
 
   def create
-    @user = User.new(params[:user])
-    if @user.save
-      sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+    if signed_in?
+      redirect_to root_path
     else
-      render 'new'
+       @user = User.new(params[:user])
+       if @user.save
+          # Handle a successful save.
+          sign_in @user
+          flash[:success] = "Welcome to the Sample App!"
+          redirect_to @user
+       else
+           render 'new'
+       end     
     end
   end
 
@@ -36,9 +47,13 @@ class UsersController < ApplicationController
   end
   
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_url
+    if current_user.admin?
+      User.find(params[:id]).destroy
+      flash[:success] = "User destroyed."
+      redirect_to users_url
+    else
+      redirect_to users_url
+    end
   end
   
   def index
@@ -47,12 +62,12 @@ class UsersController < ApplicationController
 
   private
 
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
+#    def signed_in_user
+#      unless signed_in?
+#        store_location
+#        redirect_to signin_url, notice: "Please sign in."
+#      end
+#   end
 
     def correct_user
       @user = User.find(params[:id])
